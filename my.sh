559b12b9446f5b55f955bb645073ffd2db8d56f1
mgh3326 <mgh3326@naver.com>
2018-03-03 16:54:05 +0900

    if update2

--- my.sh
+++ my.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 git pull origin master
 git add *
-if [ "$#" -eq 1 ]
+if [ $# -eq 1 ]
 then # 매개 변수가 한개 일때 (띄어쓰기가 없는 경우)
 
     git commit -m "$1"
 
-elif [ "$#" -gt 1  ]# 띄어쓰기 있는 경우
+elif [ $# -gt 1 ] # 띄어쓰기 있는 경우
 then
         for a
     do
