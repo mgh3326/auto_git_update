f84fee676b7f16aa573c107bc687be4f0b80e363
mgh3326 <mgh3326@naver.com>
2018-03-03 16:52:53 +0900

    update

--- my.sh
+++ my.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 git pull origin master
 git add *
-if [ "$#" -eq 1 ]; then # 매개 변수가 한개 일때 (띄어쓰기가 없는 경우)
+if [ "$#" -eq 1 ]
+then # 매개 변수가 한개 일때 (띄어쓰기가 없는 경우)
 
     git commit -m "$1"
 
